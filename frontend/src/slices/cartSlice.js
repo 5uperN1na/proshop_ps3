ab61c27082122bd1f1ef68e92ea557b4d79a8a1f
Paula Suarez <pauladzuck@hotmail.com>
2024-03-08 19:18:39 -0600

    updates 3824

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,36 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")): {cartItems: []};
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [] };
 
-const cartSlice = createSlice ({
+const addDecimals = (num) => {
+    return (Math.round(num * 100)/100).toFixed(2);
+}
+
+const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) => x._id ===
+                    existItem._id ? item : x)
+            } else {
+                state.cartItems = [...state.cartItems, item];
+            }
+
+            //calculate item's price
+            state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
+
+
+            //calculate shipping price
+            //calculate tax price
+            //calculate total price
+        },
+    },
 });
 
 export default cartSlice.reducer;
