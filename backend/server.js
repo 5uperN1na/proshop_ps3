da9dd949d8895a87ae3816e74c1dd11ad690088e
Paula Suarez <pauladzuck@hotmail.com>
2024-04-02 17:28:14 -0500

    Updates on april 2

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ connectDB();
 
 const app = express();
 
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res)=>{
     res.send('API is running.')
 
