8e4a2dfe17f0f42382f2b9d2617fd761663ac362
Paula Suarez <pauladzuck@hotmail.com>
2024-04-22 16:27:43 -0500

    Updates for april 22nd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import {Container } from 'react-bootstrap';
+import {Outlet} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import {Outlet} from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 import React from 'react'
@@ -16,6 +18,7 @@ const App = () => {
       </Container>
     </main>
     <Footer />
+    <ToastContainer />
     </>
      
      
