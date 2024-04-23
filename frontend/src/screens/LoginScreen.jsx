f3167d330fe2ed4e2e8843eac5dff6ec4f47fe0b
Paula Suarez <pauladzuck@hotmail.com>
2024-04-23 13:55:04 -0500

    Updates for april 23rd

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -5,11 +5,24 @@ import { useDispatch, useSelector } from "react-redux";
 import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
 import {useLoginMutation} from '../slices/usersApiSlice';
+import {setCredentials} from '../slices/authSlice';
+import {toast} from 'react-toastify';
 
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, {isLoading}] = useLoginMutation();
+
+    const {userInfo} = useSelector((state) => state.auth);
+
+    const {search} = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect');
 
     const submitHandler = (e) => {
         e.preventDefault();
