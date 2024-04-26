562dc77911f3aff0c50189cc390df4939a9edc0d
Paula Suarez <pauladzuck@hotmail.com>
2024-04-26 17:32:33 -0500

    Updates for april 26

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import {Container } from 'react-bootstrap';
 import {Outlet} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 
 import React from 'react'
