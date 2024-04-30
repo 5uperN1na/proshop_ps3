a7ce2861186dceb610014858bc75665c635e7338
Paula Suarez <pauladzuck@hotmail.com>
2024-04-30 09:38:57 -0500

    Updates april 30th

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import {logout} from '../slices/authSlice';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
@@ -9,6 +12,9 @@ const Header = () => {
 
 const {cartItems} = useSelector((state) => state.cart);
 const {userInfo} = useSelector((state) => state.auth);
+
+const dispatch = useDispatch();
+const navigate = useNavigate();
 
 const logoutHandler = () => {
   console.log("logout");
