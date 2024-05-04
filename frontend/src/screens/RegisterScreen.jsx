fb259b715526fa118d1495ba16ce09ffb5b0f98d
Paula Suarez <pauladzuck@hotmail.com>
2024-05-03 19:57:10 -0500

    Updates for May 3rd

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -54,8 +54,19 @@ const RegisterScreen = () => {
 
     return (
         <FormContainer>
-            <h1>Sign in</h1>
+            <h1>Sign Up</h1>
             <Form onSubmit={submitHandler}>
+
+            <Form.Group controlId='name' className="my-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder="Enter name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
                 <Form.Group controlId='email' className="my-3">
                     <Form.Label>Email Adress</Form.Label>
                     <Form.Control
@@ -65,6 +76,7 @@ const RegisterScreen = () => {
                         onChange={(e) => setEmail(e.target.value)}>
                     </Form.Control>
                 </Form.Group>
+
                 <Form.Group controlId='password' className="my-3">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
@@ -72,6 +84,16 @@ const RegisterScreen = () => {
                         placeholder="Enter password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='confirmPassword' className="my-3">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder="Confirm password"
+                        value={password}
+                        onChange={(e) => setConfirmPassword(e.target.value)}>
                     </Form.Control>
                 </Form.Group>
 
