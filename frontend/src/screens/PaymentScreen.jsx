b3c1c41b93a43fc7d9fa0458c3472e1d0199dbf7
Paula Suarez <pauladzuck@hotmail.com>
2024-05-16 10:57:28 -0500

    Updates for may 16th a.m.

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,14 +1,38 @@
-import {useState} from 'react';
-import {Form, Button, Col} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Col } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { savePaymentMethod } from '../slices/cartSlice';
 
 
 const PaymentScreen = () => {
-    const [paymentMethod, setPaymentMethod] = useState('PayPal');
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
 
   return (
-    <div>PaymentScreen</div>
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label as='legend'>Select Method</Form.Label>
+          <Col>
+          <Form.Check
+          type='radio'
+          className='my-2'
+          label="PayPal or Credit Card"
+          id='PayPayl'
+          name='paymentMethod'
+          value='PayPal'
+          checked
+          onChange={(e) => setPaymentMethod(e.target.value)}
+          ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type='submit' variant='primary'>Continue</Button>
+      </Form>
+    </FormContainer>
   )
 }
 
