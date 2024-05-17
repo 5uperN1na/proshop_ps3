c3c6602ffbe90b4a9fb785a2c869b6f514acd155
Paula Suarez <pauladzuck@hotmail.com>
2024-05-17 11:17:14 -0500

    Updates am may 17th

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -10,11 +10,29 @@ import { savePaymentMethod } from '../slices/cartSlice';
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState('PayPal');
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const {shippingAddress} = cart;
+
+  useEffect (() => {
+    if(!shippingAddress) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate('/placeholder');
+  }
+
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as='legend'>Select Method</Form.Label>
           <Col>
