677717da53a7e44c53dce2aa9d9f3db342d893fd
Paula Suarez <pauladzuck@hotmail.com>
2024-05-17 20:47:27 -0500

    Updates pm may 17th

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,50 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Order from '../models/orderModel.js';
+
+//@desc create new order
+//@route post/api/orders
+//@access private
+const addOrderItems = asyncHandler(async (req, res) => {
+    res.send('add order items')
+
+});
+
+//@desc get logged in orders
+//@route get/api/orders/myorders
+//@access private
+const getMyOrders = asyncHandler(async (req, res) => {
+    res.send('get my orders')
+
+});
+
+//@desc get order by id
+//@route get/api/orders/:id
+//@access private
+const getOrderById = asyncHandler(async (req, res) => {
+    res.send('get order by id')
+
+});
+
+//@desc update order to paid
+//@route post/api/orders/:id/pay
+//@access private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    res.send('update order to paid')
+
+});
+
+//@desc update order to delivered
+//@route post/api/orders/:id/deliver
+//@access private/admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    res.send('update order to delivered')
+
+});
+
+//@desc get all orders
+//@route get/api/orders
+//@access private/admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    res.send('update order to delivered')
+
+});
