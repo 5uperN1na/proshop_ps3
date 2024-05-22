70fbea2b3c299b7b6c8fde8ad791ae180251d40d
Paula Suarez <pauladzuck@hotmail.com>
2024-05-22 11:51:10 -0500

    Updates for may 22

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -5,7 +5,35 @@ import Order from '../models/orderModel.js';
 //@route post/api/orders
 //@access private
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send('add order items')
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items found.');
+    } else {
+        const order = new Order ({
+            orderItems: orderItems.map((x) =>({
+                ...x,
+                product: x._id,
+                _id: undefined
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+    }
 
 });
 
