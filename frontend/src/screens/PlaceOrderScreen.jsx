9a91e3389e021737c1c78cf010fbbe4d531107b3
Paula Suarez <pauladzuck@hotmail.com>
2024-06-20 17:26:03 -0500

    Updates for june 20th

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,6 +1,21 @@
-
+import { useEffect } from "react";
+import {Link, useNavigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import { Button, Row, Col, ListGroup, Image, Card} from 'react-bootstrap';
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const PlaceOrderScreen = () => {
+const navigate = useNavigate();
+const cart = useSelector((state) => state.cart);
+
+useEffect(()=> {
+  if () {
+
+  }else if () {
+    
+  }
+}, []);
+
   return (
     <div>PlaceOrderScreen</div>
   )
