afa287303d6687fbb75acba74d195ea08c97d902
Paula Suarez <pauladzuck@hotmail.com>
2024-06-24 22:16:19 -0500

    Updates for june 24th

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,8 +1,13 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Row, Col, ListGroup, Image, Card, ListGroupItem } from 'react-bootstrap';
+import {toast} from 'react-toastify';
 import CheckoutSteps from "../components/CheckoutSteps";
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import {useCreateOrderMutation} from '../slices/ordersApiSlice';
+import {clearCartItems} from '../slices/cartSlice';
 
 const PlaceOrderScreen = () => {
   const navigate = useNavigate();
@@ -21,7 +26,16 @@ const PlaceOrderScreen = () => {
 
       <CheckoutSteps step1 step2 step3 step4 />
       <Row>
-        <Col md={8}>col</Col>
+        <Col md={8}>
+        <ListGroup variant='flush'>
+          <ListGroupItem>
+            <h2>Shipping</h2>
+            <p>
+              
+            </p>
+          </ListGroupItem>
+        </ListGroup>
+        </Col>
         <Col md={4}>col</Col>
 
       </Row>
