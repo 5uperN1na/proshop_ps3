32875f1f4f0c06db2734691badf5f4d373ffb7c6
Paula Suarez <pauladzuck@hotmail.com>
2024-07-23 18:10:24 -0500

    updates for july 23rd

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,2 +1,20 @@
 import {Link, useParams} from 'react-router-dom';
 import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import {useGetOrderDetailQuery} from '../slices/ordersApiSlice';
+
+import React from 'react'
+
+const OrderScreen = () => {
+    const {id: orderId} = useParams();
+
+    const {data: order, refetch, isLoading, isError} = useGetOrderDetailQuery(orderId);
+
+
+  return (
+    <div>OrderScreen</div>
+  )
+}
+
+export default OrderScreen
