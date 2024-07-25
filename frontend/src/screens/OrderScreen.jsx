fddd7ebcac86f29f2ffd05d1b545ba8dd6243900
Paula Suarez <pauladzuck@hotmail.com>
2024-07-25 17:08:08 -0500

    Updates for july 25

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,27 +1,44 @@
-import {Link, useParams} from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import {useGetOrderDetailsQuery} from '../slices/ordersApiSlice';
+import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice';
 
 import React from 'react'
 
 const OrderScreen = () => {
-    const {id: orderId} = useParams();
+  const { id: orderId } = useParams();
 
-    const {data: order, refetch, isLoading, error} = useGetOrderDetailsQuery(orderId);
+  const { data: order, refetch, isLoading, error } = useGetOrderDetailsQuery(orderId);
 
-  return 
-    isLoading ? <Loader /> : error ? <Message variant='danger' /> : (
-      <>
+  return
+  isLoading ? <Loader /> : error ? <Message variant='danger' /> : (
+    <>
       <h1>Order {order._id}</h1>
       <Row>
-        <Col md={8}>Column</Col>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroupItem>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong> {order.user.email}
+              </p>
+              <p>
+                <strong>Street Address: </strong> {order.shippingAddress.address}, {order.shippingAddress.city}
+              </p>
+
+            </ListGroupItem>
+
+          </ListGroup>
+        </Col>
         <Col md={4}>Column</Col>
       </Row>
-      </>
-    ); 
-  
+    </>
+  );
+
 };
 
 export default OrderScreen
