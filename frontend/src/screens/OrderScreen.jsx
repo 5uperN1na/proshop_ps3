048a9af699491290fad1f4891913e73f2807cb54
Paula Suarez <pauladzuck@hotmail.com>
2024-08-02 13:42:30 -0500

    Updates for aug 2

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -42,6 +42,32 @@ const OrderScreen = () => {
 
             </ListGroupItem>
 
+              <ListGroup.Item>
+                <h2>Payment Method</h2>
+                <p>
+                  <strong>Method: </strong>
+                  {order.paymentMethod}
+                </p>
+
+                {order.isPaid ? (
+                <Message variant = 'success'>
+                  Paid on {order.paidAt}
+                </Message>
+              ) : (
+                <Message variant = 'danger'>Not Paid</Message>
+
+              )}
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <h2>Order Items</h2>
+                {order.orderItems.map((item, index) =>())}
+              </ListGroup.Item>
+
+
+
+
+
           </ListGroup>
         </Col>
         <Col md={4}>Column</Col>
